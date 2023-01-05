6c4b7fd6a69898b85eb3725eab6d13539498c1b1
saurabh317 <saurabhsp317@gmail.com>
2023-01-05 18:00:20 +0530

    modification -props

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import Expenseitem from "./components/ExpenseItem";
+import Expenses from './components/EXPENSES/Expenses';
+
 function App() {
   const expenses = [
     {
-      id: "1",
-      title: "books",
-      amount: "$040",
-      date: new Date(2022, 4, 19)
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "2",
-      title: "laundry",
-      amount: "$032",
-      date: new Date(2022, 5, 11)
-    },
-    {
-      id: "3",
-      title: "dinner",
-      amount: "$048",
-      date: new Date(2022, 7, 26)
-    },
-    {
-      id: "4",
-      title: "movies",
-      amount: "$980",
-      date: new Date(2022, 8, 16)
-    },
-    {
-      id: "5",
-      title: "shopping",
-      amount: "$400",
-      date: new Date(2022, 12, 12)
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
- 
-  console.log(expenses[0].date);
+
   return (
     <div>
-      <h2>lets get started</h2>
-      <Expenseitem title={expenses[0].title} amount ={expenses[0].amount} date = {expenses[0].date}></Expenseitem>
-      <Expenseitem title={expenses[1].title} amount ={expenses[1].amount} date = {expenses[1].date}></Expenseitem>
-      <Expenseitem title={expenses[2].title} amount ={expenses[2].amount} date = {expenses[3].date}></Expenseitem>
-      <Expenseitem title={expenses[3].title} amount ={expenses[3].amount} date = {expenses[3].date}></Expenseitem>
-      <Expenseitem title={expenses[4].title} amount ={expenses[4].amount} date = {expenses[4].date}></Expenseitem>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
